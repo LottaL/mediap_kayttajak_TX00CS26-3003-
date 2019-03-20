20a0878f55d29470c151da68967203166bf9bab1
LottaL <patriksigfrids@gmail.com>
2019-03-21 00:03:20 +0200

    CatCard working

--- my-app/src/components/CatCard.js
+++ my-app/src/components/CatCard.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import '../App.css';
+import TR from './TR.js';
 
 class CatCard extends Component {
   render() {
     console.log(this.props.catcards);
     return this.props.catcards.map((card) => (
-       <h3>{catcards.title}</h3>
+       <TR card={card}/>
     ));
   }
 }
---
+++ my-app/src/components/TR.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+export class TR extends Component {
+  render() {
+    return (
+        <tr>
+          <td>
+            <img src={this.props.card.thumbnails.w160} alt="Title"/>
+          </td>
+          <td>
+            <h3>{this.props.card.title}</h3>
+            <p>{this.props.card.description}</p>
+          </td>
+          <td>
+            <a href="#">View</a>
+          </td>
+        </tr>
+    )
+  }
+}
+
+export default TR
